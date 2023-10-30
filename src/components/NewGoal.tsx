7e0fc3cb13cf4d1c23fd0dabbd1471fa130b799b
Sean Tolbert <sean@seans-mbp.mynetworksettings.com>
2023-10-30 15:51:27 -0400

    completes todo app with working forms, adding, and deleting functionality

---
+++ src/components/NewGoal.tsx
@@ -0,0 +1,35 @@
+import { useRef, type FormEvent } from "react";
+
+type NewGoalProps = {
+  onAddGoal: (goal: string, summary: string) => void;
+};
+
+export default function NewGoal({ onAddGoal }: NewGoalProps) {
+  const goal = useRef<HTMLInputElement>(null);
+  const summary = useRef<HTMLInputElement>(null);
+
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const enteredGoal = goal.current!.value;
+    const enteredSummary = summary.current!.value;
+
+    e.currentTarget.reset();
+    onAddGoal(enteredGoal, enteredSummary);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <p>
+        <label htmlFor="goal">Your Goal</label>
+        <input type="text" id="goal" ref={goal} />
+      </p>
+      <p>
+        <label htmlFor="summary">Short Summary</label>
+        <input type="text" id="summary" ref={summary} />
+      </p>
+      <p>
+        <button>Add Goal</button>
+      </p>
+    </form>
+  );
+}
